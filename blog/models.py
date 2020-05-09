eb0e4310fff2e04d35b1401692982588cc6ca1e4
AustinWilliam117 <mengxu1996@gmail.com>
2020-05-09 23:23:04 +0800

    5.9

--- blog/models.py
+++ blog/models.py
@@ -4,16 +4,44 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
 class Tag(models.Model):
     name = models.CharField(max_length=20)
 
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
+    
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
-    title = models.CharField(max_length=70)
-    body = models.TextField()
-    created_time = models.DateTimeField()
+    title = models.CharField('标题',max_length=70)
+    body = models.TextField('正文')
+    created_time = models.DateTimeField('创建时间',auto_now_add=True)
     # 最后一次修改日期
-    modified_time = models.DateTimeField()
-    excerpt = models.CharField(max_length=200, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag, blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    modified_time = models.DateTimeField('修改时间',auto_now_add=True)
+    excerpt = models.CharField('摘要',max_length=200, blank=True)
+    category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, verbose_name='标签', blank=True)
+    author = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        self.modified_time = timezone.now()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 def index(request):
@@ -6,3 +6,10 @@ def index(request):
     context['title'] = '我的网站'
     context['welcome'] = '您好，欢迎来到我的网站'
     return render(request, 'blog/index.html', context)
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    context = {}
+    context['title'] = title
+    context['post'] = post
+    return render(request, 'blog/detail.html', context)
