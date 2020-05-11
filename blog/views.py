3da94c6f990309547747729c83c99362df118df5
AustinWilliam117 <mengxu1996@gmail.com>
2020-05-11 20:55:06 +0800

    5.11.2

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 from django.utils.text import slugify
 from markdown.extensions.toc import TocExtension
 
+from .models import Post, Category, Tag
+
 from .models import Post
 
 def index(request):
@@ -29,3 +31,24 @@ def detail(request, pk):
 
     context['post'] = post
     return render(request, 'blog/detail.html', context)
+
+# 归档
+def archive(request, year, month):
+    archive_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+# 分类
+def category(request, pk):
+    # 记得在开始部分导入 Category 类
+    cate = get_object_or_404(Category, pk=pk)
+    category_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+# 标签
+def tag(request, pk):
+    # 记得在开始部分导入 Tag 类
+    t = get_object_or_404(Tag, pk=pk)
+    tag_list = Post.objects.filter(tags=t).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
