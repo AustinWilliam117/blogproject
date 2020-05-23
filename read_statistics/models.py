8841baa7f043e19acdf39ab2ff73d817a7f6be13
AustinWilliam117 <mengxu1996@gmail.com>
2020-05-23 11:35:06 +0800

    5.23.1

--- read_statistics/models.py
+++ read_statistics/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db.models.fields import exceptions
+from django.utils import timezone
 
 class ReadNum(models.Model):
-    read_num = models.IntegerField(default=0)
+    read_num = models.IntegerField('阅读数', default=0)
 
     content_type = models.ForeignKey(ContentType, on_delete=models.DO_NOTHING)
     object_id = models.PositiveIntegerField()
@@ -22,5 +23,18 @@ class ReadNumExpandMethod:
             return readnum.read_num
         except exceptions.ObjectDoesNotExist:
             return 0
+
+class ReadDetail(models.Model):
+    date = models.DateField('日期', default=timezone.now)
+    read_num = models.IntegerField('阅读数', default=0)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.DO_NOTHING)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    
+    class Meta:
+        verbose_name = '阅读详细统计'
+        verbose_name_plural = verbose_name
+
 
 
--- read_statistics/utils.py
+++ read_statistics/utils.py
@@ -1,5 +1,6 @@
 from django.contrib.contenttypes.models import ContentType
-from .models import ReadNum
+from django.utils import timezone
+from .models import ReadNum, ReadDetail
 
 def read_statistics_once_read(request, obj):
     ct = ContentType.objects.get_for_model(obj)
@@ -7,12 +8,17 @@ def read_statistics_once_read(request, obj):
 
     # 阅读统计
     if not request.COOKIES.get(key):
-        if ReadNum.objects.filter(content_type=ct, object_id=obj.pk).count():
-            # 如果存在记录
-            readnum = ReadNum.objects.get(content_type=ct, object_id=obj.pk)
-        else:
-            readnum = ReadNum(content_type=ct, object_id=obj.pk)
+        # 总阅读数 +1
+        readnum, created = ReadNum.objects.get_or_create(content_type=ct, object_id=obj.pk)
         readnum.read_num += 1
         readnum.save()
+
+        # 当天阅读数 +1
+        date = timezone.now().date()
+        readDetail, created = ReadDetail.objects.get_or_create(content_type = ct, object_id = obj.pk, date=date)
+        readDetail.read_num += 1
+        readDetail.save()
     return key
 
+def get_week_read_data(content_type):
+    pass
